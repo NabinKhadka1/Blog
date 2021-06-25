c0055ddec76b2c97ecafd75605be9cee09d05b81
NabinKhadka1 <nabinkhadka330@gmail.com>
2021-06-25 16:30:43 +0545

    Static files fix

--- blog_project/settings/base.py
+++ blog_project/settings/base.py
@@ -98,17 +98,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_ROOT = BASE_DIR.parent/ 'staticfiles'
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [BASE_DIR.parent/ 'static']
-
+STATIC_ROOT = BASE_DIR.parent/ 'staticfiles'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.parent/ 'static']
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+# STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
+
+# STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
